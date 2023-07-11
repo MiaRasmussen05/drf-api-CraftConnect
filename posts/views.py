c722024c40b55772bd805c7127345c05ebc20dba
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-07-11 04:17:31 +0000

    Add search feature and filter feature for posts

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,7 @@
 from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, permissions, filters
 from .models import Post
-from comments.models import Comment
-from likes.models import Like
 from .serializers import PostSerializer
 from crafthub_api.permissions import IsOwnerOrReadOnly
 
@@ -21,7 +20,18 @@ class PostList(generics.ListCreateAPIView):
         likes_count=Count('likes', distinct=True)
     ).order_by('-created_at')
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
     ]
     ordering_fields = [
         'likes__created_at',
