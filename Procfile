51e7b81b80e8958b4f143613528d86fca1594fb6
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-07-12 04:26:11 +0000

    set up for deployment  with procfile, env and update settings

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py makemigrations && python manage.py migrate
+web: gunicorn crafthub_api.wsgi
--- crafthub_api/settings.py
+++ crafthub_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists('env.py'):
     import env
@@ -46,6 +47,7 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'crafthub_api.serializers.CurrentUserSerializer'
@@ -55,13 +57,13 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ipvbo&1oi-3az5)4d)ga+env#+lb$pw55fygzxy23qv-md(sll'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '8000-miarasmusse-drfapicraft-98xbcs8mgzw.ws-eu101.gitpod.io']
-
+# ALLOWED_HOSTS = ['localhost', '8000-miarasmusse-drfapicraft-98xbcs8mgzw.ws-eu101.gitpod.io']
+ALLOWED_HOSTS = ['localhost', 'drf-api-crafthub.herokuapp.com']
 
 # Application definition
 
@@ -83,6 +85,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
+    'corsheaders',
 
     'profiles',
     'posts',
@@ -92,6 +95,7 @@ INSTALLED_APPS = [
 ]
 SITE_ID = 1
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -100,6 +104,23 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if 'CLIENT_ORIGIN' in os.environ:
+     CORS_ALLOWED_ORIGINS = [
+         os.environ.get('CLIENT_ORIGIN')
+    ]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\.gitpod\.io$",
+    ]
+
+"""
+Enable sending cookies in cross-origin requests so 
+that users can get authentication functionality
+"""
+
+CORS_ALLOW_CREDENTIALS = True
+
 
 ROOT_URLCONF = 'crafthub_api.urls'
 
@@ -125,12 +146,17 @@ WSGI_APPLICATION = 'crafthub_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 
 # Password validation
