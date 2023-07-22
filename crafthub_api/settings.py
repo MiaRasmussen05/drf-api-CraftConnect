24312ace2a16b18c382a251e9e202a3ec49a33ed
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-07-22 03:27:48 +0000

    Create modelfor todo tasks

--- crafthub_api/settings.py
+++ crafthub_api/settings.py
@@ -96,6 +96,7 @@ INSTALLED_APPS = [
     'comments',
     'likes',
     'followers',
+    'todo',
 ]
 SITE_ID = 1
 MIDDLEWARE = [
---
+++ todo/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Idea(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+
+class TaskCategory(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    idea = models.ForeignKey(Idea, related_name='todo_items', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_rgq6aq', blank=True
+    )
+    category = models.ForeignKey(TaskCategory, blank=True, null=True, on_delete=models.SET_NULL)
+    completed_percentage = models.IntegerField(default=0)
+    completed = models.BooleanField(default=False, blank=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
