c448f1827808205ab1dd87e7259aa474090a1b50
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-07-22 16:21:22 +0000

    Add serializer for the idea, category and task in todo

---
+++ todo/serializers.py
@@ -0,0 +1,60 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from .models import Idea, TaskCategory, Task
+
+
+class IdeaSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Idea
+        fields = [
+            'owner', 'is_owner', 'created_at',
+            'title', 'description'
+        ]
+
+
+class TaskCategorySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = TaskCategory
+        fields = 'name'
+    
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    category = TaskCategorySerializer()
+    
+    
+    def validate_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        return value
+
+    class Meta:
+        model = Task
+        fields = [
+            'title', 'content', 'created_at',
+            'image', 'category', 'completed_percentage',
+            'completed'
+        ]
