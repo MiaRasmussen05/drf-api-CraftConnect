7fb3abdd133f15eee8cc0ae9e238355ba595da95
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-07-27 17:49:31 +0000

    Fix allowed hosts in settings

--- crafthub_api/settings.py
+++ crafthub_api/settings.py
@@ -66,8 +66,8 @@ DEBUG = 'DEV' in os.environ
 
 
 
-# ALLOWED_HOSTS = ['localhost', '8000-miarasmusse-drfapicraft-ho4bvk2wduz.ws-eu102.gitpod.io']
 ALLOWED_HOSTS = ['localhost', '8000-miarasmusse-drfapicraft-ho4bvk2wduz.ws-eu102.gitpod.io']
+# ALLOWED_HOSTS = ['localhost', '8000-miarasmusse-drfapicraft-ho4bvk2wduz.ws-eu102.gitpod.io']
 
 # Application definition
 
